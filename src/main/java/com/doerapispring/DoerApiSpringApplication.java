27293ed9a33126ea305fa11293a724e48adcbd07
Chirag Tailor <ctailor2@gmail.com>
2017-02-22 21:26:56 -0600

    Set system timezone to ensure timestamps are set correctly

--- src/main/java/com/doerapispring/DoerApiSpringApplication.java
+++ src/main/java/com/doerapispring/DoerApiSpringApplication.java
@@ -3,10 +3,18 @@ package com.doerapispring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DoerApiSpringApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(DoerApiSpringApplication.class, args);
+	}
+
+    // TODO: This is too global. Move closer to the database.
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 }
