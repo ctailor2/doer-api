cee69a2f8d0aa154cda9f738fa8a66ea010535f3
Chirag Tailor <ctailor2@gmail.com>
2019-05-08 14:29:52 -0500

    Upgrade to spring boot 2, flyway 5, and gradle 4.4

--- api/src/test/java/com/doerapispring/web/AuthenticatedControllerTest.java
+++ api/src/test/java/com/doerapispring/web/AuthenticatedControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,6 +24,7 @@ public class AuthenticatedControllerTest {
     private AuthenticatedUser authenticatedUser;
     private AuthenticatedController authenticatedController;
 
+    @RestController
     static class AuthenticatedController {
         @RequestMapping(value = "/authenticated-endpoint", method = RequestMethod.GET)
         @ResponseBody
