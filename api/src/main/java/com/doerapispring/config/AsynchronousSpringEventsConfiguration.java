0371bad22d10edcae995546a0e971caaf86000fa
Chirag Tailor <ctailor2@gmail.com>
2019-10-01 22:13:20 -0500

    Disable async spring events configuration for test

--- api/src/main/java/com/doerapispring/config/AsynchronousSpringEventsConfiguration.java
+++ api/src/main/java/com/doerapispring/config/AsynchronousSpringEventsConfiguration.java
@@ -2,11 +2,13 @@ package com.doerapispring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
+@Profile("!test")
 public class AsynchronousSpringEventsConfiguration {
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
