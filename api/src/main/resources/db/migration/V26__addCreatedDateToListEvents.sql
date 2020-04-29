cd35be441a6b8fc40abf1bb07d11a6f3745d25cd
Chirag Tailor <ctailor2@gmail.com>
2020-04-29 16:45:05 -0500

    Update migration to account for the fact that some todo completed events have their completed timestamps stored as iso timestamps (from migration) and others (from the app) as big integers

--- api/src/main/resources/db/migration/V26__addCreatedDateToListEvents.sql
+++ api/src/main/resources/db/migration/V26__addCreatedDateToListEvents.sql
@@ -1,9 +1,21 @@
 ALTER TABLE list_events ADD COLUMN created_at timestamp without time zone NOT NULL default CURRENT_TIMESTAMP;
 
 CREATE TEMPORARY TABLE todo_completed_events AS
-SELECT user_id, list_id, version, data::json->>'completedTodoId' AS todo_id, (data::json->>'completedAt')::timestamp AS completed_at
-FROM list_events
-WHERE event_class = 'com.doerapispring.domain.events.TodoCompletedEvent';
+SELECT user_id, list_id, version, todo_id, completed_at_text::timestamp AS completed_at
+FROM (
+    SELECT user_id, list_id, version, data::json->>'completedTodoId' AS todo_id, data::json->>'completedAt' AS completed_at_text
+    FROM list_events
+    WHERE event_class = 'com.doerapispring.domain.events.TodoCompletedEvent'
+) AS sub_todo_completed_events
+WHERE completed_at_text ~ '\d{2}-\d{2}-\d{2}.+'
+UNION
+SELECT user_id, list_id, version, todo_id, to_timestamp(completed_at_text::bigint::double precision / 1000) AS completed_at
+FROM (
+    SELECT user_id, list_id, version, data::json->>'completedTodoId' AS todo_id, data::json->>'completedAt' AS completed_at_text
+    FROM list_events
+    WHERE event_class = 'com.doerapispring.domain.events.TodoCompletedEvent'
+) AS sub_todo_completed_events
+WHERE completed_at_text !~ '\d{2}-\d{2}-\d{2}.+';
 
 UPDATE list_events
 SET created_at = todo_completed_events.completed_at
