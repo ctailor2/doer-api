f70d8322a2f707b839074398c7b4c76a8108f886
Chirag Tailor <ctailor2@gmail.com>
2020-08-24 22:15:24 -0500

    Repopulate first snapshot, ensuring to populate the sectionName and deferredSectionName

---
+++ api/src/main/resources/db/migration/V29__repopulateFirstSnapshot.sql
@@ -0,0 +1,16 @@
+TRUNCATE TABLE todo_lists;
+
+INSERT INTO todo_lists
+SELECT
+    user_identifier AS user_id,
+    uuid AS list_id,
+    json_build_object(
+        'listId', json_build_object('name', uuid),
+        'profileName', name,
+        'todos', json_build_array(),
+        'lastUnlockedAt', 0,
+        'demarcationIndex', 0,
+        'sectionName', 'now',
+        'deferredSectionName', 'later') AS data,
+    TIMESTAMP 'epoch' AS created_at
+FROM lists;
